de9f56d8565964a6bbf903c1e7c4fcb68e7e2dc4
yassine grairi <grairiiyassine@gmail.com>
2026-07-26 04:23:40 +0100

    make cv btn circular

--- components/download-cv.tsx
+++ components/download-cv.tsx
@@ -19,12 +19,12 @@ export default function DownloadCvButton() {
             borderRadius={999}
             backgroundOpacity={0}
             saturation={1.8}
-            className="px-3 py-2 max-sm:px-1.5 max-sm:py-1 max-lg:px-4 max-lg:py-2.5 cursor-pointer"
+            className="aspect-square p-2 max-sm:p-1 max-lg:p-2.5 cursor-pointer"
           >
             <a
               href="/cv.pdf"
               download
-              className="flex items-center justify-center p-1.5 rounded-lg aspect-square text-neutral-700 dark:text-neutral-300"
+              className="flex items-center justify-center p-1.5 text-neutral-700 dark:text-neutral-300"
             >
               <Download size={24} />
             </a>
